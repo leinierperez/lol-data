bc88238f29bea93c6a34a5d06fb234b4ca0b6b72
Leinier Perez <P_leinier@yahoo.com>
2023-07-02 22:16:19 -0400

    Scrape all quotes without categorizing by skins and remove duplicates

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const scrapeChampionQuotes = async (champion) => {
     x(`https://leagueoflegends.fandom.com/wiki/${champion}/LoL/Audio`, {
       quotes: x('.mw-parser-output ul li', [
         {
-          skin: x(['.skin-play-button@data-skin']),
           quote: x('i'),
-          url: x(['.audio-button audio@src']),
-          url2: x(['.ext-audiobutton source@src']),
+          url: x('.audio-button audio@src'),
+          url2: x('.ext-audiobutton source@src'),
         },
       ]),
     })((err, obj) => {
@@ -20,25 +19,20 @@ const scrapeChampionQuotes = async (champion) => {
         reject(err);
       } else {
         const quotes = [];
-        const seen = new Set();
         for (const q of obj.quotes) {
           const url = q.url.length > 0 ? q.url : q.url2;
           if (url.length === 0) continue;
           const quote = q.quote?.replace(/[^\w\s]/g, '');
           if (quote === 'Sound Effect') continue;
-          const skins = q.skin.length > 0 ? q.skin : ['Original'];
-          skins.forEach((skin, index) => {
-            const key = `${skin}-${quote}`;
-            if (seen.has(key)) return;
-            seen.add(key);
-            quotes.push({
-              skin,
-              quote,
-              url: url[index]?.split('/revision')[0],
-            });
+          quotes.push({
+            quote,
+            url: url.split('/revision')[0],
           });
         }
-        resolve({ name: champion, quotes });
+        const uniqueQuotes = [
+          ...new Map(quotes.map((q) => [q.quote, q])).values(),
+        ];
+        resolve({ name: champion, uniqueQuotes });
       }
     });
   });
